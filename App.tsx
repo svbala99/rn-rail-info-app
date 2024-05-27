1d6c60798657bb112fba8ecf0318149b325f33bf
Balamurugan V <balamurugan.v@softway.com>
2024-05-28 02:08:04 +0530

    final commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import * as React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import WebView from 'react-native-webview';
+import { ActivityIndicator, Dimensions, View } from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+function HomeScreen() {
+  const [load, setLoad] = React.useState(true)
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex:1}}>
+      <WebView onLoad={()=>setLoad(false)} source={{ uri: 'https://indiarailinfo.com/' }} style={{ flex: 1 }} />
+      {load && <ActivityIndicator
+          style={{ position: "absolute", top: Dimensions.get('window').height / 2, left: Dimensions.get('window').width / 2 }}
+          size="large"
+        />}
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+function SettingsScreen() {
+  const [load, setLoad] = React.useState(true)
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View style={{flex:1}}>
+      <WebView onLoad={()=>setLoad(false)} source={{ uri: 'https://www.trainman.in/coach-position' }} style={{ flex: 1 }} />
+      {load && <ActivityIndicator
+          style={{ position: "absolute", top: Dimensions.get('window').height / 2, left: Dimensions.get('window').width / 2 }}
+          size="large"
+        />}
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+function TrackingScreen() {
+  const [load, setLoad] = React.useState(true)
+  return (
+    <View style={{flex:1}}>
+      <WebView onLoad={()=>setLoad(false)}  source={{ uri: 'https://m.etrain.info/live-train-status' }} style={{ flex: 1 }} />
+      {load && <ActivityIndicator
+          style={{ position: "absolute", top: Dimensions.get('window').height / 2, left: Dimensions.get('window').width / 2 }}
+          size="large"
+        />}
+    </View>
+  );
+}
 
-export default App;
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="ரயில்கள்" component={HomeScreen} options={{
+          tabBarIcon: ({ size, color }) =>
+            <MaterialCommunityIcons name="train" size={size} color={color} />,
+        }} />
+      <Tab.Screen name="பெட்டிகள்" component={SettingsScreen} options={{
+          tabBarIcon: ({ size, color }) =>
+            <FontAwesome5 name="boxes" size={size} color={color} />,
+        }} />
+        <Tab.Screen name="நேரலை" component={TrackingScreen} options={{
+          tabBarIcon: ({ size, color }) =>
+            <MaterialCommunityIcons name="google-maps" size={size} color={color} />,
+        }} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
